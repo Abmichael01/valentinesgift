def589e92dd5a4d372d618633d17bd4c86ec28d5
Urkel <urkelcodes7@gmail.com>
2024-02-14 08:14:26 +0100

    new commit

--- script.js
+++ script.js
@@ -7,80 +7,80 @@ const c = document.querySelector(".box-cover")
 
 const stop = document.querySelector(".stop")
 
+box.addEventListener("click", ()=>{
+
+    box.classList.add("big")
+    
+    const audio = document.getElementById("myAudio");
+    audio.pause()
+    audio.play();
+
+    stop.style.display = "inline-block"
+    stop.addEventListener("click", ()=>{
+        stopAudio(audio)
+    })
+
+    setTimeout(()=>{
+        supriseCont.style.display = "flex"
+        welcome.style.display = "none"
+    }, 300)
+})
 
 
+// function updateCountdown() {
+    
+//     const now = new Date();
+    
+    
+//     const nextTomorrow = new Date(now);
+//     nextTomorrow.setDate(now.getDate() + 1);
+//     nextTomorrow.setHours(0, 0, 0, 0); 
+    
+    
+//     const difference = nextTomorrow - now;
+    
+    
+//     if(difference <= 0){
+        
+//     }else{
+//         const hours = Math.floor(difference / (1000 * 60 * 60));
+//         const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+//         const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+        
+        
+//         const countdownElement = document.querySelector(".countdown");
+//         countdownElement.innerHTML = `${hours}:${minutes}:${seconds}`;
 
-function updateCountdown() {
-    
-    const now = new Date();
-    
-    
-    const nextTomorrow = new Date(now);
-    nextTomorrow.setDate(now.getDate() + 1);
-    nextTomorrow.setHours(0, 0, 0, 0); 
-    
-    
-    const difference = nextTomorrow - now;
-    
-    
-    if(difference <= 0){
-        box.addEventListener("click", ()=>{
-
-            box.classList.add("big")
+//         box.addEventListener("click", ()=>{
+//             const audio = document.getElementById("myAudio");
+//             audio.play();
             
-            const audio = document.getElementById("myAudio");
-            audio.pause()
-            audio.play();
+//             stop.style.display = "inline-block"
 
-            stop.style.display = "inline-block"
-            stop.addEventListener("click", ()=>{
-                stopAudio(audio)
-            })
+//             stop.addEventListener("click", ()=>{
+//                 stopAudio(audio)
+//             })
 
-            setTimeout(()=>{
-                supriseCont.style.display = "flex"
-                welcome.style.display = "none"
-            }, 300)
-        })
-    }else{
-        const hours = Math.floor(difference / (1000 * 60 * 60));
-        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-        
-        
-        const countdownElement = document.querySelector(".countdown");
-        countdownElement.innerHTML = `${hours}:${minutes}:${seconds}`;
+//             const msg = document.querySelector(".msg");
+//             msg.classList.add("error")
 
-        box.addEventListener("click", ()=>{
-            const audio = document.getElementById("myAudio");
-            audio.play();
-            
-            stop.style.display = "inline-block"
+//             if(msg.classList.contains("error")){
+//                 setTimeout(()=>{
+//                     msg.classList.remove("error")
+//                 }, 5000)
+//             }
 
-            stop.addEventListener("click", ()=>{
-                stopAudio(audio)
-            })
-
-            const msg = document.querySelector(".msg");
-            msg.classList.add("error")
-
-            if(msg.classList.contains("error")){
-                setTimeout(()=>{
-                    msg.classList.remove("error")
-                }, 5000)
-            }
-
-        })
+//         })
 
         
-    }
-  }
+//     }
+//   }
   
  
-  updateCountdown();
+//   updateCountdown();
   
 
-  setInterval(updateCountdown, 1000);
+//   setInterval(updateCountdown, 1000);
 
   function stopAudio(audio){
     audio.pause()
